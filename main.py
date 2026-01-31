2041f0ff2c1d298a06cdd6e0ea6434cbcbf93b29
Minka-glitch <minkathefox@gmail.com>
2026-02-01 12:21:47 +1300

    Player added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from circleshape import CircleShape
 
 def main():
     pygame.init()
@@ -8,15 +10,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while True:
+    while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
-
+        player.draw(screen)
+        pygame.display.flip()        
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
 
